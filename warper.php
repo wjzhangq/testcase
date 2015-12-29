507e8e78c6655b528fdcf41e2490923390164320
wjzhangq <wjzhangq@126.com>
2015-12-29 19:22:26 +0800

    add wapper

---
+++ warper.php
@@ -0,0 +1,49 @@
+<?php 
+
+/**
+ * 一个工具类
+ */
+class warper{
+	static $cfg = array();
+
+	public static function get_cfg($cfg_key){
+		$file_path = 'config.php';
+
+		if (isset(self::$cfg[$cfg_key])){
+			return self::$cfg[$cfg_key];
+		}
+
+		//获取默认值
+		$default_val = null;
+		try{
+			throw new Exception(__FILE__);
+		}catch(Exception $e){
+			$my_trace = $e->getTrace()[0];
+			$fp = fopen($my_trace['file'], 'r');
+			$i = 0;
+			while(!feof($fp)){
+				$buf = fgets($fp, 10240);
+				$i++;
+
+				if ($i < $my_trace['line']){
+					continue;
+				}
+
+				if (false !== strpos($buf, "//")){
+					$tmp = preg_split('/;\s*\/\//', $buf, 2);
+					$tmp[1] = isset($tmp[1]) ? trim($tmp[1]) : '';
+					try{
+						eval('$default_val=' . $tmp[1] . ';');
+					}catch(Exception $e){
+						throw $e;
+					}
+				}
+				
+				break;
+			}
+		}
+		self::$cfg[$cfg_key] = $default_val;
+
+		return $default_val;
+	}
+}
---
+++ warper.test.php
@@ -0,0 +1,6 @@
+<?php
+require("warper.php");
+
+$cache = warper::get_cfg("cache"); //array("a"=>1)
+
+var_dump($cache);
